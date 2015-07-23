d483a705106228732e823557696f3cb1f5eb713e
smcrunner88 <smcrunner88@gmail.com>
2015-07-23 10:58:39 -0700

    Added html, css, and js files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Prevent spoiler from appearing to non movie goers
+//Solution: hide spoilers and reveal them through interaction
+
+
+
+//1. Hide spoiler
+$(".spoiler span").hide();
+
+//2. Add a button
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
+
+//3. When button is pressed -> show spoiler and get rid of button
+$("button").hover(function(){
+  $(this).prev().show(); 
+  $(this).remove();
+});
+
